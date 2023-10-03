301fba56c902813b11c96a360e15245969591fb8
hcc00101 <hcc00101@gmail.com>
2023-10-03 21:40:28 +0800

    Filter Games By Genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,10 +3,15 @@ import GameCard from "./GameCard";
 import useGames from "../hooks/useGames";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameGardContainer from "./GameGardContainer";
+import { Genre } from "../hooks/useGenres";
 
-const GameGrid = () => {
-  const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5];
+interface Props {
+  selectedGenre: Genre | null;
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre);
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
