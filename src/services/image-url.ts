dea569af87dbbc03b76736d3d9bfbd4b3809427b
hcc00101 <hcc00101@gmail.com>
2023-10-08 11:38:06 +0800

    Build Sort Selector

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
 const getCroppedImageUrl = (url: string) => {
   const target = "media/";
-  const index = url.indexOf(target) + target.length;
-  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+  const index = url !== null ? url.indexOf(target) + target.length : -1;
+  return index !== -1
+    ? url.slice(0, index) + "crop/600/400/" + url.slice(index)
+    : url;
 };
 
 export default getCroppedImageUrl;
